e4c566a34cea5b609fd6f0ac4557a606be4d8ef4
James Vanneman <jvannem@gmail.com>
2012-10-21 12:18:57 -0400

    cleaned up files

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,10 @@
 class Song
-	attr_accessor :genre, :name
+	attr_accessor :genre, :name, :artist
 	
+	def initialize
+		@genre = Genre.new
+	end
+
 	def genre=(genre)
 		@genre = genre
 		@genre.songs << self	
